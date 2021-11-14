9e3570879a83324a6a1e73975ea820124f36e3eb
jitasek <jitka.moreira@gmail.com>
2021-11-14 23:43:24 +0100

    add input prompts

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,57 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter your name: ",
-            name: "Name",
-        },
-        {
-            type: "input",
-            message: "Enter your bio: ",
-            name: "Bio",
-        },
-        {
-            type: "input",
-            message: "Enter your github link: ",
-            name: "GitHub",
-        },
-        {
-            type: "input",
-            message: "Enter your linkedin URL: ",
-            name: "LinkedIn",
-        },
-    
-    ])
-];
+// TODO: Create an array of questions for user input::
+//sections entitled , , Usage, License, Contributing, Tests, and Questions
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter project title: ",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "Enter project description: ",
+      name: "Description",
+    },
+
+    {
+      type: "input",
+      message: "Enter installation instructions: ",
+      name: "Installation",
+    },
+    {
+      type: "input",
+      message: "Enter usage info: ",
+      name: "Usage",
+    },
+    {
+      type: "input",
+      message: "Enter contribution guidelines: ",
+      name: "Contributors",
+    },
+    {
+      type: "input",
+      message: "Enter tests instructions: ",
+      name: "Tests",
+    },
+    {
+      type: "input",
+      message: "Enter your GitHub URL: ",
+      message: "Enter your email: ",
+      name: "Questions",
+    },
+    // {
+    //   type: "checkbox",
+    //   message: "Select the license: ",
+    //   name: "License",
+    //   choices: ["MIT", "GPL", "BSD"],
+    // },
+  ])
+  .then(function (answers) {
+    console.log("Answers: ", answers.Title, answers.Description);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
