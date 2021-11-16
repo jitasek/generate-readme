6063163230ba81538e421b1c97e6ab9e4b4d992d
jitasek <jitka.moreira@gmail.com>
2021-11-16 17:56:37 +0100

    take questions out of inquirerAsync

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,63 +4,68 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input::
 
-// set up an async function (3 steps)
-const inquirerAsync = async () => {
-  // 1 step: declare the questions
-  const questions = [
-    {
-      type: "input",
-      message: "Enter project title: ",
-      name: "Title",
-    },
-    {
-      type: "input",
-      message: "Enter project description: ",
-      name: "Description",
-    },
+const questions = [
+  {
+    type: "input",
+    message: "Enter project title: ",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "Enter project description: ",
+    name: "Description",
+  },
 
-    // {
-    //   type: "input",
-    //   message: "Enter installation instructions: ",
-    //   name: "Installation",
-    // },
-    // {
-    //   type: "input",
-    //   message: "Enter usage info: ",
-    //   name: "Usage",
-    // },
-    // {
-    //   type: "input",
-    //   message: "Enter contribution guidelines: ",
-    //   name: "Contributors",
-    // },
-    // {
-    //   type: "input",
-    //   message: "Enter tests instructions: ",
-    //   name: "Tests",
-    // },
-    // {
-    //   type: "input",
-    //   message: "Enter your GitHub URL: ",
-    //   message: "Enter your email: ",
-    //   name: "Questions",
-    // },
-    // {
-    //   type: "checkbox",
-    //   message: "Select the license: ",
-    //   name: "License",
-    //   choices: ["MIT", "GPL", "BSD"],
-    // },
-  ];
+  // {
+  //   type: "input",
+  //   message: "Enter installation instructions: ",
+  //   name: "Installation",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Enter usage info: ",
+  //   name: "Usage",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Enter contribution guidelines: ",
+  //   name: "Contributors",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Enter tests instructions: ",
+  //   name: "Tests",
+  // },
+  // {
+  //   type: "input",
+  //   message: "Enter your GitHub URL: ",
+  //   message: "Enter your email: ",
+  //   name: "Questions",
+  // },
+  // {
+  //   type: "checkbox",
+  //   message: "Select the license: ",
+  //   name: "License",
+  //   choices: ["MIT", "GPL", "BSD"],
+  // },
+];
 
-  // 2 step: prompt the questions to get answers (once complete, it will return me the answers)
-  const answers = await inquirer.prompt(questions); // anynchronous operation, so I have to await for it to finish
+// set up an async function (3 steps) - prep the function to accept questions
+const inquirerAsync = async (questions) => {
+  // 1 step: declare the questions (moved outside the function - above)
+  try {
+    // 2 step: prompt the questions to get answers (once complete, it will return me the answers)
+    const answers = await inquirer.prompt(questions); // anynchronous operation, so I have to await for it to finish
 
-  // 3 step: log the answers
-  console.log(answers);
+    // 3 step: log the answers
+    console.log(answers);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(0); // exits the code
+  }
 };
 
-inquirerAsync();
+inquirerAsync(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
