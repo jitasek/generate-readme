ca1877f6a3ba325c72d4dda280feb8bff4ca264e
jitasek <jitka.moreira@gmail.com>
2021-11-16 15:24:34 +0100

    refactor for async manner

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input::
-//sections entitled , , Usage, License, Contributing, Tests, and Questions
-inquirer
-  .prompt([
+
+// set up an async function (3 steps)
+const inquirerAsync = async () => {
+  // 1 step: declare the questions
+  const questions = [
     {
       type: "input",
       message: "Enter project title: ",
@@ -17,42 +19,48 @@ inquirer
       name: "Description",
     },
 
-    {
-      type: "input",
-      message: "Enter installation instructions: ",
-      name: "Installation",
-    },
-    {
-      type: "input",
-      message: "Enter usage info: ",
-      name: "Usage",
-    },
-    {
-      type: "input",
-      message: "Enter contribution guidelines: ",
-      name: "Contributors",
-    },
-    {
-      type: "input",
-      message: "Enter tests instructions: ",
-      name: "Tests",
-    },
-    {
-      type: "input",
-      message: "Enter your GitHub URL: ",
-      message: "Enter your email: ",
-      name: "Questions",
-    },
+    // {
+    //   type: "input",
+    //   message: "Enter installation instructions: ",
+    //   name: "Installation",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter usage info: ",
+    //   name: "Usage",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter contribution guidelines: ",
+    //   name: "Contributors",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter tests instructions: ",
+    //   name: "Tests",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter your GitHub URL: ",
+    //   message: "Enter your email: ",
+    //   name: "Questions",
+    // },
     // {
     //   type: "checkbox",
     //   message: "Select the license: ",
     //   name: "License",
     //   choices: ["MIT", "GPL", "BSD"],
     // },
-  ])
-  .then(function (answers) {
-    console.log("Answers: ", answers.Title, answers.Description);
-  });
+  ];
+
+  // 2 step: prompt the questions to get answers (once complete, it will return me the answers)
+  const answers = await inquirer.prompt(questions); // anynchronous operation, so I have to await for it to finish
+
+  // 3 step: log the answers
+  console.log(answers);
+};
+
+inquirerAsync();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
